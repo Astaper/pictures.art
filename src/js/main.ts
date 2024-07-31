d87aea033ae043a7be8671fd64fa2c5b94200049
Astaper <astapowitsch@gmail.com>
2024-07-31 22:17:23 +0300

    sliders

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,19 @@
 import modals from "./modules/modals";
+import sliders from "./modules/sliders";
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
+
+    sliders({
+        slides: '.feedback-slider-item',
+        dir: 'horizontal',
+        prev: '.main-prev-btn',
+        next: '.main-next-btn',
+    });
+    sliders({
+        slides: '.main-slider-item',
+        dir: 'vertical',
+        prev: '',
+        next: '',
+    });
 });
--- src/js/modules/sliders.ts
+++ src/js/modules/sliders.ts
@@ -1,4 +1,83 @@
-const sliders = () => {
+interface ISlidesOptions {
+    slides: string;
+    dir: string;
+    prev: string;
+    next: string;
+}
 
+const sliders = ({ slides, dir, prev, next }: ISlidesOptions) => {
+    let slideIndex = 1;
+    let paused = false;
+    let intervalId: number;
+    const slide = document.querySelectorAll<HTMLElement>(slides);
+
+
+    const showSlides = (n: any) => {
+        if (n > slide.length) {
+            slideIndex = 1;
+        }
+
+        if (n < 1) {
+            slideIndex = slide.length;
+        }
+
+        slide.forEach(item => {
+            item.classList.add("animated");
+            item.style.display = "none";
+        });
+
+        slide[slideIndex - 1].style.display = 'block';
+    }
+
+    showSlides(slideIndex);
+
+    const plusSlides = (n: any) => {
+        showSlides(slideIndex += n);
+    }
+
+    try {
+        const prevBtn = document.querySelector(prev);
+        const nextBtn = document.querySelector(next);
+
+        prevBtn?.addEventListener('click', () => {
+            plusSlides(-1);
+            slide[slideIndex - 1].classList.remove('slideInLeft');
+            slide[slideIndex - 1].classList.add('slideInRight');
+        });
+
+        nextBtn?.addEventListener('click', () => {
+            plusSlides(1);
+            slide[slideIndex - 1].classList.remove('slideInRight');
+            slide[slideIndex - 1].classList.add('slideInLeft');
+        });
+    } catch (e) { }
+
+    const activateAnimation = () => {
+        if (dir === 'vertical') {
+            intervalId = setInterval(function () {
+                plusSlides(1);
+                slide[slideIndex - 1].classList.add('slideInDown');
+            }, 3000);
+        } else {
+            intervalId = setInterval(function () {
+                plusSlides(1);
+                slide[slideIndex - 1].classList.remove('slideInRight');
+                slide[slideIndex - 1].classList.add('slideInLeft');
+            }, 3000);
+        }
+        paused = false;
+    }
+
+    activateAnimation();
+
+
+    slide[0].parentNode?.addEventListener('mouseenter', () => {
+        clearInterval(intervalId);
+        paused = true;
+    });
+    slide[0].parentNode?.addEventListener('mouseleave', () => {
+        activateAnimation();
+    });
 };
+
 export default sliders;
