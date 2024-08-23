69bc46730580250a07834b380d5b3b9ee97a27da
Astaper <astapowitsch@gmail.com>
2024-08-23 18:32:08 +0300

    !

---
+++ src/js/modules/calc.ts
@@ -0,0 +1,9 @@
+const calc = (size, material, options, promocode, result) => {
+    const sizeBlock = document.querySelector(size);
+    const materilBlock = document.querySelector(material);
+    const optionsBlock = document.querySelector(options);
+    const promocodeBlock = document.querySelector(promocode);
+    const resultBlock = document.querySelector(result);
+};
+
+export default calc;
