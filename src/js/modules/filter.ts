98905f28a22fc9d5d06e8fd8f5d620f65db6f268
Astaper <astapowitsch@gmail.com>
2024-08-31 12:12:37 +0300

    !

---
+++ src/js/modules/filter.ts
@@ -0,0 +1,20 @@
+const filter = () => {
+    const menu = document.querySelector('.portfolio-menu');
+    const items = document.querySelectorAll('.li');
+    const btnAll = document.querySelector('.all');
+    const btnLovers = document.querySelector('.lovers');
+    const btnChef = document.querySelector('.chef');
+    const btnGirl = document.querySelector('.girl');
+    const btnGuy = document.querySelector('.guy');
+    const btnGrandmother = document.querySelector('.grandmother');
+    const btngranddad = document.querySelector('.granddad');
+    const wrapper = document.querySelector('.porfolio-wrapper');
+    const markAll = document.querySelectorAll('.all');
+    const markGirl = document.querySelectorAll('.girl');
+    const markLovers = document.querySelectorAll('.lovers');
+    const markChef = document.querySelectorAll('.chef');
+    const markGuy = document.querySelectorAll('.guy');
+    const no = document.querySelector('.portfolio-no');
+};
+
+export default filter;
