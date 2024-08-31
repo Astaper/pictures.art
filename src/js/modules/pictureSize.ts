d88eb4ecec31d95a44919a0ea798aef0e9acd2ac
Astaper <astapowitsch@gmail.com>
2024-08-31 15:04:29 +0300

    !

--- src/js/modules/pictureSize.ts
+++ src/js/modules/pictureSize.ts
@@ -1,13 +1,26 @@
-const pictureSize = (imgSelector) => {
-    const blocks = document.querySelectorAll(imgSelector);
+const pictureSize = (imgSelector: string) => {
+    const blocks = document.querySelectorAll(imgSelector) as NodeListOf<HTMLElement>;
 
-    const showImg = (block) => {
-        const img = block.querySelector('img');
-        img.src = img.src.slice(0, -4) + '-1.png';
-        block.querySelectorAll('p').forEach(p => {
-            p.style.display = 'none';
-        });
+    const showImg = (block: HTMLElement) => {
+        const img = block.querySelector('img') as HTMLImageElement | null;
+        if (img) {
+            img.src = img.src.slice(0, -4) + '-1.png';
+            block.querySelectorAll('p:not(.sizes-hit)').forEach(p => (p as HTMLElement).style.display = 'none');
+        }
     }
+
+    const hideImg = (block: HTMLElement) => {
+        const img = block.querySelector('img') as HTMLImageElement | null;
+        if (img) {
+            img.src = img.src.slice(0, -6) + '.png';
+            block.querySelectorAll('p:not(.sizes-hit)').forEach(p => (p as HTMLElement).style.display = 'block');
+        }
+    }
+
+    blocks.forEach(block => {
+        block.addEventListener('mouseover', () => showImg(block));
+        block.addEventListener('mouseout', () => hideImg(block));
+    });
 };
 
 export default pictureSize;
