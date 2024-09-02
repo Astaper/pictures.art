23b53b4f97fff8c43768513f0528d8d1830ee7ff
Astaper <astapowitsch@gmail.com>
2024-09-02 13:04:26 +0300

    !

--- src/js/modules/accordion.ts
+++ src/js/modules/accordion.ts
@@ -16,26 +16,6 @@ const accordion = (triggersSelector: string) => {
             }
         });
     });
-
-
-
-    // const blocks = document.querySelectorAll(itemsSelector);
-
-    // blocks.forEach(block => {
-    //     block.classList.add('animated', 'fadeInDown');
-    // });
-
-    // btns.forEach(btn => {
-    //     btn.addEventListener('click', function () {
-    //         if (!this.classList.contains('active')) {
-    //             btns.forEach(btn => {
-    //                 btn.classList.remove('active', 'active-style');
-    //             });
-    //             this.classList.add('active', 'active-style');
-    //         }
-    //     });
-    // });
-
 };
 
 export default accordion;
